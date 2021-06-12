5edf808efa782120d2a63313ec2cd50d500cd458
Noah Zarro <noahzarro@gmail.com>
2021-06-12 18:01:59 +0200

    background and 12agon

--- generate_slices.py
+++ generate_slices.py
@@ -1,6 +1,6 @@
 from math import sin, cos, pi
 
-base_string = '<mask id="mask{seg_nr}">\n<!-- Only stuff under a white pixel will be visible -->\n<path d="M {center_x}, {center_y} l {out_x} {out_y} A {radius} {radius} {degree} 0 1 {end_x} {end_y}" fill="white"></path>\n</mask>'
+base_string = '<mask id="mask{seg_nr}">\n<!-- Only stuff under a white pixel will be visible -->\n<path d="M {center_x}, {center_y} l {out_x} {out_y} L {end_x} {end_y}" fill="white"></path>\n</mask>'
 radius = 500
 center_x = 960
 center_y = 540
@@ -13,6 +13,7 @@ def delta_x(angle):
 def delta_y(angle):
     return radius*cos(angle)
 
+ends = []
 
 for i in range(12):
     out_x = delta_x(i*rad)
@@ -24,7 +25,17 @@ for i in range(12):
     end_x = center_x + delta_x((i+1)*rad)
     end_y = center_y - delta_y((i+1)*rad)
 
+    ends.append((end_x, end_y))
+
     segment_mask = base_string.format(seg_nr = i, center_x=center_x, center_y=center_y, out_x=out_x, out_y=out_y, radius=radius, degree=degree, end_x=end_x, end_y=end_y)
 
     print(segment_mask)
 
+
+
+print("<path d=\"M {start_x} {start_y}".format(start_x=center_x+delta_x(0), start_y=center_y-delta_y(0) ))
+
+for end_x, end_y in ends:
+    print("L {end_x} {end_y}".format(end_x=end_x, end_y=end_y))
+
+print("\" stroke=\"black\"></path>")
